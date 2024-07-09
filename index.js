05a70c6ad2afb4753b25e1b50291cdd6ccef48b6
shivamvishwakarm <vishwakarmashivam2003@gmail.com>
2024-07-10 01:58:42 +0530

    add vercel configuration

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const port = 3000;
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname, './public'))); // Serve static files from the "public" directory
+app.use(express.json());
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../public/index.html'));
@@ -20,19 +21,16 @@ app.get('/', function(req, res) {
 const results = JSON.parse(fs.readFileSync(path.join(__dirname, 'results.json'), 'utf-8'));
 
 // Search endpoint
-app.get('/search', function(req, res) {
-    var nameQuery = req.query.name;
-    console.log(nameQuery)
+app.get('/api/search', function(req, res) {
+    const nameQuery = req.query.name;
     if (!nameQuery) {
         return res.status(400).json({ error: 'Name query parameter is required' });
     }
-    // Filter results by name
-    var filteredResults = results.filter(function(result) {
+    const filteredResults = results.filter(function(result) {
         return result.name.toLowerCase().includes(nameQuery.toLowerCase());
     });
     res.json(filteredResults);
 });
-
 app.listen(port, function() {
     console.log(`Server is running on http://localhost:${port}`);
 });
