3eb697c87e6b0ff568684ea0420e41921069da3c
shivamvishwakarm <vishwakarmashivam2003@gmail.com>
2024-07-10 02:05:39 +0530

    some debugging

--- index.js
+++ index.js
@@ -18,7 +18,45 @@ app.get('/', function(req, res) {
 
 // Load results data from JSON file
 // const results = JSON.parse(fs.readFileSync('results.json', 'utf-8'));
-const results = JSON.parse(fs.readFileSync(path.join(__dirname, 'results.json'), 'utf-8'));
+// const results = JSON.parse(fs.readFileSync(path.join(__dirname, 'results.json'), 'utf-8'));
+const results = [
+    {
+        "roll_no": "2220140050001",
+        "name": "ABHAY ANAND",
+        "marks": "PRACTICAL, MARKS",
+        "total_marks_3rd": "352",
+        "total_marks_hons": "560",
+        "grand_total": "943",
+        "division": "FIRST CLASS"
+      },
+      {
+        "roll_no": "2220140050002",
+        "name": "ABHISHEK KUMAR",
+        "marks": "PRACTICAL, MARKS",
+        "total_marks_3rd": "321",
+        "total_marks_hons": "505",
+        "grand_total": "899",
+        "division": "FIRST CLASS"
+      },
+      {
+        "roll_no": "2220140050003",
+        "name": "ABHISHEK KUMAR",
+        "marks": "PRACTICAL, MARKS",
+        "total_marks_3rd": "333",
+        "total_marks_hons": "553",
+        "grand_total": "953",
+        "division": "FIRST CLASS"
+      },
+      {
+        "roll_no": "2220140050004",
+        "name": "ABHISHEK SINGH",
+        "marks": "PRACTICAL, MARKS",
+        "total_marks_3rd": "325",
+        "total_marks_hons": "516",
+        "grand_total": "862",
+        "division": "FIRST CLASS"
+      },
+]
 
 // Search endpoint
 app.get('/api/search', function(req, res) {
